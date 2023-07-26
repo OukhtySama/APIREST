3fbb169858c1c077837d64834fa2520a68a5db42
OukhtySama <allanv69330@gmail.com>
2023-07-26 12:58:35 +0200

    Renvoie vetements auto

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const { v4: uuid } = require("uuid")
 const app = express();
 
 app.get("/outfit", (req, res) => {
-    res.send("This is working§");
+    const tops = ["Black", "White", "Orange", "Navy"];
+    const jeans = ["Grey", "Dark Grey", "Black", "Navy"];
+    const shoes = ["White", "Grey", "Black", "Red"];
+
+    res.json({
+        top: _.sample(tops),
+        jeans: _.sample(jeans),
+        shoes: _.sample(shoes)
+    });
 });
 app.listen(3000, () => console.log("API Server s running..."));
